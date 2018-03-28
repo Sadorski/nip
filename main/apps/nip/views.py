64e943d372971d1464fac6a11985f9a35abe0b43
Sultan Sultani <sadorski@Sultans-MacBook-Pro.local>
2018-03-28 15:48:27 -0700

    initial commit

---
+++ main/apps/nip/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, HttpResponse, redirect
+from models import *
+  # the index function is called when root is visited
+def index(request):
+    response = "Hello, I am your first request!"
+    return HttpResponse(response)
+
+def process(request):
+    pass
