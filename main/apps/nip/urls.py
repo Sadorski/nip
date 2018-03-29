fcfa4a2afc6b84f141d576f7fca4a180f411079c
Tejas Konduru <Tejas@Vanaja-Kondurus-MacBook-Air-3.local>
2018-03-29 16:54:58 -0700

    added socials

--- main/apps/nip/urls.py
+++ main/apps/nip/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from . import views           # This line is new!
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^process/?$', views.process),
     url(r'^new_user/?$', views.new_user),      
     url(r'^login/?$', views.login),
-    url(r'^strengths/?$', views.strength)     # This line has changed!
+    url(r'^strengths/?$', views.strength),
+    url(r'^socials/?$', views.socials),
+    url(r'^socials_process/?$', views.socials_process),
 ]
--- main/apps/nip/views.py
+++ main/apps/nip/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, HttpResponse, redirect
 from models import *
+import bcrypt
+import re
+from django.contrib import messages 
   # the index function is called when root is visited
 def index(request):
     return render(request, 'nip/login.html')
-
-
 
 def new_user(request):
 	errors = User.objects.regis_basic_validator(request.POST)
@@ -19,7 +20,7 @@ def new_user(request):
 	password = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
 	User.objects.create(first_name=first_name, last_name=last_name, email=email, image=image,password=password)
 	request.session['id'] = User.objects.get(email=email).id
-	return redirect('/')
+	return redirect('/socials')
 
 def login(request):
 	errors = User.objects.log_basic_validator(request.POST)
@@ -41,4 +42,43 @@ def strength(request):
         'stacks': Stack.objects.all()
     }
     return render(request, 'nip/strengths.html', context)
+
+def socials(request):
+	print request.session['id']
+	return render(request, 'nip/registration.html')
+
+def socials_process(request):
+	errors = {}
+	if len(request.POST['linkedin']) < 1:
+		errors['linkedin'] = 'LinkedIn field is mandatory :)'
+	# if len(request.POST['type']) < 1:
+	# 	errors['post'] = 'Please answer if you are an instructor or student'
+	if len(request.POST['github']) < 1:
+		errors['github'] = 'GitHub field is mandatory :)'
+	if len(errors):
+		for tag, error in errors.iteritems():
+			messages.error(request, error, extra_tags=tag)
+		return redirect('/socials')
+	status = request.POST['type']
+	linkedin = request.POST['linkedin']
+	facebook = request.POST['facebook']
+	github = request.POST['github']
+	slack = request.POST['slack']
+	instagram = request.POST['instagram']
+	twitter = request.POST['twitter']
+	user = User.objects.get(id=request.session['id'])
+	user.status = status
+	user.linkedin = linkedin
+	user.facebook = facebook
+	user.github = github
+	user.slack = slack
+	user.instagram = instagram
+	user.twitter = twitter
+ 	return redirect('/socials')
+
+
+
+
+
+
 
