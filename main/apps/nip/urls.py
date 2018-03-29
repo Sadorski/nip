119494b35ade06f1723c29a071a10813b3e0fe60
Sultan Sultani <sadorski@Sultans-MacBook-Pro.local>
2018-03-29 16:08:04 -0700

    tejas changes

--- main/apps/nip/urls.py
+++ main/apps/nip/urls.py
@@ -3,5 +3,7 @@ from . import views           # This line is new!
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^process/?$', views.process),
+    url(r'^new_user/?$', views.new_user),      
+    url(r'^login/?$', views.login),
     url(r'^strengths/?$', views.strength)     # This line has changed!
 ]
